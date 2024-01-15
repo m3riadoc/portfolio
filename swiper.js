95c3415144dbdda57f443196e3fced055fe63b71
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2024-01-15 20:34:31 +0100

    commit

--- swiper.js
+++ swiper.js
@@ -1,10 +1,33 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 10,
+  slidesPerView: 4,
+  spaceBetween: 0,
   loop: true,
   autoplay: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    600: {
+      slidesPerView: 3,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 3.5,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
   },
 });
